00a472dfd81560801dc0cf901a4a23192433bd73
edriso <edriso.dev@gmail.com>
2023-02-09 14:52:41 +0200

    add 404 response

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const taskRouter = require('./routes/taskRoutes.js');
 const connectDB = require('./db/connect');
-require('dotenv').config();
+const notFound = require('./middleware/not-found');
 
 // middleware
 app.use(express.static('./public'));
 app.use(express.json());
 
+// routes
 app.use('/api/v1/tasks', taskRouter);
+app.use(notFound);
 
 const port = 3000;
 const start = async () => {
